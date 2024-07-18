7993c99920eac4223928432738cf67fa33ca1136
dermirez <dermirez@gmail.com>
2024-07-18 14:09:45 -0500

    First commit: front end structure

---
+++ front/scripts/tempData.js
@@ -0,0 +1,42 @@
+const tempData = [
+  {
+    title: "Guardians of the Galaxy Vol. 2",
+    year: 2017,
+    director: "James Gunn",
+    duration: "2h 16min",
+    gender: ["Action", "Adventure", "Comedy"],
+    rate: 7.7,
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg",
+  },
+  {
+    title: "Star Wars: Episode IV - A New Hope",
+    year: 1977,
+    director: "George Lucas",
+    duration: "2h 1min",
+    gender: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
+    rate: 8.7,
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
+  },
+  {
+    title: "The Lord of the Rings: The Fellowship of the Ring",
+    year: 2001,
+    director: "Peter Jackson",
+    duration: "2h 58min",
+    gender: ["Action", "Adventure", "Drama", "Fantasy"],
+    rate: 8.8,
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
+  },
+  {
+    title: "The King Lion",
+    year: 1994,
+    director: "Roger Allers",
+    duration: "1h 28min",
+    gender: ["Musical", "Drama", "Adventure", "Family"],
+    rate: 8.9,
+    poster:
+      "https://lumiere-a.akamaihd.net/v1/images/p_thelionking_19752_1_0b9de87b.jpeg?region=0%2C0%2C540%2C810",
+  },
+];
